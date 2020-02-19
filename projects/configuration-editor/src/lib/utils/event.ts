d997a1b0da4a888dec5c4c133fc4407595980faf
MrHuangJser <565628147@qq.com>
2020-02-19 14:21:52 +0800

    MrHuangJser - improve event util

--- projects/configuration-editor/src/lib/utils/event.ts
+++ projects/configuration-editor/src/lib/utils/event.ts
@@ -2,3 +2,5 @@ import { fromEvent } from 'rxjs';
 
 export const pointermoveEvent$ = fromEvent<PointerEvent>(document, 'pointermove');
 export const pointerupEvent$ = fromEvent<PointerEvent>(document, 'pointerup');
+export const keydownEvent$ = fromEvent<KeyboardEvent>(document, 'keydown');
+export const keyupEvent$ = fromEvent<KeyboardEvent>(document, 'keyup');
