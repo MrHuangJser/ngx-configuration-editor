1b161d27cbca3d5cb9e74cef2f94d8b54290985e
MrHuangJser <565628147@qq.com>
2020-02-19 14:20:29 +0800

    MrHuangJser - add coordinates service

---
+++ projects/configuration-editor/src/lib/services/coordinates.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { ConfigurationEditorService } from '../configuration-editor.service';
+import { EditorStore } from './editor.store';
+
+@Injectable()
+export class CoordinatesService {
+  constructor(private store: EditorStore, private editorSrv: ConfigurationEditorService) {}
+
+  private getEditorElement(): HTMLElement {
+    return document.querySelector(`[editor-id=${this.editorSrv.editorId}]`);
+  }
+
+  clientToEditor(x: number, y: number): [number, number] {
+    const ele = this.getEditorElement();
+    const { left, top } = ele.getBoundingClientRect();
+    return [x - left, y - top];
+  }
+
+  editorToCanvas(x: number, y: number): [number, number] {
+    const { left, top } = this.store.getValue();
+    return [x - left, y - top];
+  }
+
+  clientToCanvas(x: number, y: number) {
+    return this.editorToCanvas(...this.clientToEditor(x, y));
+  }
+}
