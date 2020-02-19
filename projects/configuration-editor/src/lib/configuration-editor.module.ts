03e5f4c5c9e66442544892877869bfdf44c0e78d
MrHuangJser <565628147@qq.com>
2020-02-19 14:22:33 +0800

    MrHuangJser - improve imports

--- projects/configuration-editor/src/lib/configuration-editor.module.ts
+++ projects/configuration-editor/src/lib/configuration-editor.module.ts
@@ -1,16 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { GridComponent } from './components/grid/grid.component';
 import { NoZoomAreaComponent } from './components/no-zoom-area/no-zoom-area.component';
 import { ConfigurationEditorComponent } from './configuration-editor.component';
 import { DragDirective } from './directives/drag.directive';
 import { SelectorDirective } from './directives/selector.directive';
+import { EditorStoreQuery } from './services/editor-query.service';
+import { EditorStore } from './services/editor.store';
+import { CoordinatesService } from './services/coordinates.service';
 
 const COMPONENTS = [ConfigurationEditorComponent, NoZoomAreaComponent, GridComponent];
 const DIRECTIVES = [DragDirective, SelectorDirective];
+const SERVICES = [EditorStoreQuery, EditorStore, CoordinatesService];
 
 @NgModule({
   declarations: [...COMPONENTS, ...DIRECTIVES],
-  imports: [],
+  imports: [CommonModule],
+  providers: [...SERVICES],
   exports: [ConfigurationEditorComponent]
 })
 export class ConfigurationEditorModule {}
