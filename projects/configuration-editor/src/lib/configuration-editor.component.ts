95b8327eed0fabab3c12756d929a12322b5fd767
MrHuangJser <565628147@qq.com>
2020-02-19 14:22:22 +0800

    MrHuangJser - improve editor body

--- projects/configuration-editor/src/lib/configuration-editor.component.ts
+++ projects/configuration-editor/src/lib/configuration-editor.component.ts
@@ -1,12 +1,15 @@
-import { Component, OnInit, ChangeDetectionStrategy, ChangeDetectorRef, Input, Output, EventEmitter } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, EventEmitter, HostBinding, HostListener, Input, OnInit, Output } from '@angular/core';
+import { ConfigurationEditorService } from './configuration-editor.service';
 import { ViewConfig } from './interface';
+import { CoordinatesService } from './services/coordinates.service';
+import { EditorStore } from './services/editor.store';
 
 @Component({
   selector: 'ce-configuration-editor',
   templateUrl: './configuration-editor.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ConfigurationEditorComponent {
+export class ConfigurationEditorComponent implements OnInit {
   @Input('ceConfig')
   set viewConfig(value: ViewConfig) {
     this._viewConfig = value;
@@ -15,8 +18,47 @@ export class ConfigurationEditorComponent {
     return this._viewConfig;
   }
   @Output('ceOnInit') initEmitter = new EventEmitter<any>();
+  @HostBinding('attr.editor-id') editorId: string;
 
   private _viewConfig: ViewConfig;
+  private dragStartPoints: [number, number] | null = null;
 
-  constructor(public cdr: ChangeDetectorRef) {}
+  constructor(
+    public cdr: ChangeDetectorRef,
+    public editorSrv: ConfigurationEditorService,
+    private store: EditorStore,
+    private coordinatesSrv: CoordinatesService
+  ) {}
+
+  ngOnInit() {
+    this.editorId = this.editorSrv.editorId = `editor_${Date.now()}${Math.round(Math.random() * 100000)}`;
+    this.cdr.detectChanges();
+  }
+
+  setDragStartPoints(ev: PointerEvent | null) {
+    if (ev) {
+      const { left, top } = this.store.getValue();
+      this.dragStartPoints = [left, top];
+    } else {
+      this.dragStartPoints = null;
+    }
+  }
+
+  dragMoving([mx, my]) {
+    if (this.dragStartPoints) {
+      const [x, y] = this.dragStartPoints;
+      console.log(x, y);
+      this.editorSrv.moveCanvas(x + mx, y + my);
+    }
+  }
+
+  @HostListener('wheel', ['$event'])
+  scale(e: WheelEvent & { wheelDelta: number }) {
+    if (e.metaKey) {
+      e.preventDefault();
+      e.stopPropagation();
+      const wheelDelta = e.wheelDelta / 120 || -e.deltaY / 3;
+      this.editorSrv.scaleCanvas(wheelDelta * 0.05, this.coordinatesSrv.clientToEditor(e.clientX, e.clientY));
+    }
+  }
 }
--- projects/configuration-editor/src/lib/configuration-editor.service.ts
+++ projects/configuration-editor/src/lib/configuration-editor.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
-import { fromEvent } from 'rxjs';
+import { action } from '@datorama/akita';
+import { EditorStore } from './services/editor.store';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ConfigurationEditorService {
-  constructor() {}
+  public editorId: string;
+  constructor(private store: EditorStore) {}
+
+  @action('ce-editor:moveCanvas')
+  moveCanvas(x: number, y: number) {
+    this.store.update({ left: x, top: y });
+  }
+
+  @action('ce-editor:scaleCanvas')
+  scaleCanvas(delta: number, center: [number, number] | null) {
+    const { scale, left, top } = this.store.getValue();
+    const [x, y] = center || [left, top];
+    const newScale = scale + delta;
+    if (newScale <= 4 && newScale >= 0.2) {
+      this.store.update({ scale: newScale, left: (left - x) * (delta / scale) + left, top: (top - y) * (delta / scale) + top });
+    }
+  }
 }
