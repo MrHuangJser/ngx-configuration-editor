ddfd090c2b3971843bc70e2cefff1a7f362646a7
MrHuangJser <565628147@qq.com>
2020-02-19 13:26:11 +0800

    MrHuangJser - add example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { ConfigurationEditorModule } from 'projects/configuration-editor/src/public-api';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, ConfigurationEditorModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
