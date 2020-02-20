37a42d25b7d5dbd5c3f9cb4245febd326291345c
MrHuangJser <565628147@qq.com>
2020-02-20 19:40:25 +0800

    MrHuangJser - add example data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,24 @@
 import { Component } from '@angular/core';
+import { ConfigurationEditorService } from 'projects/configuration-editor/src/public-api';
+import * as config from '../../config.json';
+import { IEditorState } from 'projects/configuration-editor/src/lib/services/editor.store';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent {}
+export class AppComponent {
+  public editorSrv: ConfigurationEditorService;
+  public viewConfig: IEditorState;
+
+  constructor() {
+    this.viewConfig = {
+      ...config.editorTransform.style,
+      left: config.editorTransform.transform.x,
+      top: config.editorTransform.transform.y,
+      scale: config.editorTransform.transform.scale,
+      items: config.editorData.reduce((obj, item) => ({ ...obj, [item.id]: item }), {})
+    };
+  }
+}
