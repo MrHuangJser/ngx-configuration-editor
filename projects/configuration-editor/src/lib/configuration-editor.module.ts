e6e1cbcbc5de3c4bff52a27c96a35828d04ff9c6
MrHuangJser <565628147@qq.com>
2020-02-20 15:28:36 +0800

    MrHuangJser - add border component

--- projects/configuration-editor/src/lib/configuration-editor.module.ts
+++ projects/configuration-editor/src/lib/configuration-editor.module.ts
@@ -2,13 +2,14 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { AkitaNgDevtools } from '@datorama/akita-ngdevtools';
 import { environment } from 'src/environments/environment';
+import { BorderAreaComponent } from './components/border-area/border-area.component';
 import { GridComponent } from './components/grid/grid.component';
 import { NoZoomAreaComponent } from './components/no-zoom-area/no-zoom-area.component';
 import { ConfigurationEditorComponent } from './configuration-editor.component';
 import { DragDirective } from './directives/drag.directive';
 import { SelectorDirective } from './directives/selector.directive';
 
-const COMPONENTS = [ConfigurationEditorComponent, NoZoomAreaComponent, GridComponent];
+const COMPONENTS = [ConfigurationEditorComponent, NoZoomAreaComponent, GridComponent, BorderAreaComponent];
 const DIRECTIVES = [DragDirective, SelectorDirective];
 
 @NgModule({
