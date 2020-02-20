a9097e6be36a690159b5499c8059ff9789aa7082
MrHuangJser <565628147@qq.com>
2020-02-20 12:18:56 +0800

    MrHuangJser - fix no zoom area issue

--- projects/configuration-editor/src/lib/components/no-zoom-area/no-zoom-area.component.ts
+++ projects/configuration-editor/src/lib/components/no-zoom-area/no-zoom-area.component.ts
@@ -1,30 +1,15 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, HostBinding, OnDestroy } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { ChangeDetectorRef, Component, ChangeDetectionStrategy } from '@angular/core';
 import { EditorStoreQuery } from '../../services/editor-query.service';
 
 @Component({
-  selector: '[ce-no-zoom-area]',
+  selector: 'ce-no-zoom-area',
   templateUrl: './no-zoom-area.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class NoZoomAreaComponent implements OnDestroy {
-  @HostBinding('style.width.px') width: number;
-  @HostBinding('style.height.px') height: number;
-  @HostBinding('style.transform') transform: string;
+export class NoZoomAreaComponent {
+  constructor(public cdr: ChangeDetectorRef, public storeQuery: EditorStoreQuery) {}
 
-  private subscription = new Subscription();
-
-  constructor(public cdr: ChangeDetectorRef, private storeQuery: EditorStoreQuery) {
-    this.subscription.add(
-      this.storeQuery.select(['width', 'scale', 'height', 'left', 'top']).subscribe(({ width, height, scale, left, top }) => {
-        this.width = width * scale;
-        this.height = height * scale;
-        this.transform = `translate3d(${left}px,${top}px,0)`;
-      })
-    );
-  }
-
-  ngOnDestroy() {
-    this.subscription.unsubscribe();
+  getTransform(left: number, top: number) {
+    return `translate3d(${left}px,${top}px,0)`;
   }
 }
