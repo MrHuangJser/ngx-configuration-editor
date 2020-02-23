c18a09f96a85d07a8e5ae2305d5348d6fa997af3
MrHuangJser <565628147@qq.com>
2020-02-23 21:32:49 +0800

    MrHuangJser - add example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
-import { ConfigurationEditorService } from 'projects/configuration-editor/src/public-api';
-import * as config from '../../config.json';
 import { IEditorState } from 'projects/configuration-editor/src/lib/services/editor.store';
+import { AlignDirection, ConfigurationEditorService } from 'projects/configuration-editor/src/public-api';
+import * as config from '../../config.json';
 
 @Component({
   selector: 'app-root',
@@ -20,5 +20,21 @@ export class AppComponent {
       scale: config.editorTransform.transform.scale,
       items: config.editorData.reduce((obj, item) => ({ ...obj, [item.id]: item }), {})
     };
+  }
+
+  editorInit(editorSrv: ConfigurationEditorService) {
+    this.editorSrv = editorSrv;
+  }
+
+  align(direction: AlignDirection) {
+    this.editorSrv.alignItems(direction, [...this.editorSrv.selectorStore.getValue().selected]);
+  }
+
+  group() {
+    this.editorSrv.groupItems([...this.editorSrv.selectorStore.getValue().selected]);
+  }
+
+  breakUp() {
+    this.editorSrv.breakUpItem([...this.editorSrv.selectorStore.getValue().selected][0]);
   }
 }
