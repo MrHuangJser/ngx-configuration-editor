d7dd5ab8afe51d2780d2316e617b6f52121e8238
MrHuangJser <565628147@qq.com>
2020-02-23 22:57:39 +0800

    MrHuangJser - add service export

--- projects/configuration-editor/src/public-api.ts
+++ projects/configuration-editor/src/public-api.ts
@@ -2,7 +2,13 @@
  * Public API Surface of configuration-editor
  */
 
-export * from './lib/configuration-editor.service';
 export * from './lib/configuration-editor.component';
 export * from './lib/configuration-editor.module';
+export * from './lib/configuration-editor.service';
 export * from './lib/interface';
+export * from './lib/services/coordinates.service';
+export * from './lib/services/editor-query.service';
+export * from './lib/services/editor.store';
+export * from './lib/services/selector-query.service';
+export * from './lib/services/selector.store';
+export * from './lib/services/utils.service';
